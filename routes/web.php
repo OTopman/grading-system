ab427b278b7f598fcf1901aa620fd905c243f144
Tech4All <tech4all583@gmail.com>
2021-07-08 14:22:27 +0100

    initial

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('auth/login');
 });
+
+
+Route::group(['namespace'=>'auths','prefix'=>'auth'], function (){
+    Route::resource("/login", "LoginController");
+});
+
